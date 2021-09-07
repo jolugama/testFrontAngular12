7260add19960930db089f4113200f58601a8e816
Jose Luis G <garciamartinezjoseluis@gmail.com>
2021-09-07 02:36:37 +0200

    create directive ui-ripple, add module directives

---
+++ src/app/directives/directives.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UiRippleDirective } from './ui-ripple.directive';
+
+
+
+@NgModule({
+  declarations: [
+    UiRippleDirective
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class DirectivesModule { }
